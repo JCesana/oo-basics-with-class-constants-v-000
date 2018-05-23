bf748373b5081ee7d1b022c01ec4c40a9870d3ac
Josh Cesana <joshuacesana@gmail.com>
2018-05-23 19:15:46 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title
+  attr_reader :title, :genre 
   
   GENRES = []
 
@@ -10,12 +10,9 @@ class Book
   
   def genre=(genre)
     @genre= genre
+    GENRES << genre 
   end 
-  
-  def genre
-    @genre 
-  end 
-  
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
